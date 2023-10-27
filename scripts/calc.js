e3576c5a551499ac0ef467d410ea0384a5a61edb
Ritik <91615440+Ritikkhulbe@users.noreply.github.com>
2023-10-27 16:58:44 +0530

    Numbers&OperatorsApart

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,9 +1,36 @@
-function calculate(){
-    let solution = Number(document.querySelector(".disp").value);
-    console.log(solution);
+var oper = {
+    '+': (x, y) => x+y,
+    '-': (x, y) => x-y,
+    '*': (x, y) => x*y,
+    '/': (x, y) => x/y,
+    '%': (x, y) => x%y,
+    '^': (x, y) => x**y  
 }
 
+
+function calculate(para){
+    let nums = [];
+    let op = [];
+    let curr="";
+    Array.from(para).forEach(element => {
+        if((element>='0' && element<='9') || element === '.'){
+            curr+=element;
+        }else{
+            nums.push(Number(curr));
+            op.push(element);
+            curr="";
+        }
+        console.log(nums);
+        console.log(op);
+    })
+
+
+}
 function toDisplay(para) {
     document.querySelector(".disp").value +=para;
-    calculate();
+    calculate(document.querySelector(".disp").value);
+}
+
+function displayClear(){
+    document.querySelector(".disp").value = "";
 }
